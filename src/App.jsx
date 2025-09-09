5f93087ee601f95c6b574a2558ccb7eb2e00e405
Belen Romero <anab.romero@gmail.com>
2025-09-09 10:15:21 -0300

    Componentes clase 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
 import "./App.css";
-import { Boton } from "./components/Boton";
-import { Item1 } from "./components/Item1";
-import { Item2 } from "./components/Item2";
-import { Item3 } from "./components/Item3";
-import { Item4 } from "./components/Item4";
-import { Item5 } from "./components/Item5";
-import { Item6 } from "./components/Item6";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
-  const prod = { nombre: "Remera", precio: 10000, descripcion: "LALALA" };
+  //PROVISORAMENTE
+  const arrayProductos = [
+    {
+      id: 1,
+      nombre: "Remera",
+      precio: 20000,
+      descripcion: "Remera de algodon",
+    },
+    {
+      id: 2,
+      nombre: "Pantalon",
+      precio: 80000,
+      descripcion: "Pantalon de jean",
+    },
+    {
+      id: 3,
+      nombre: "zapatillas",
+      precio: 110000,
+      descripcion: "Zapatillas deportivas",
+    },
+  ];
+  //const prod = { nombre: "Remera", precio: 10000, descripcion: "LALALA" };
   return (
     <>
       <div>
-        <Boton texto={"Enviar"} color={"green"} />
-        <Boton texto={"Cancelar"} color={"red"} />
-        <Item1
-          nombre={prod.nombre}
-          precio={prod.precio}
-          descripcion={prod.descripcion}
-        >
-          <button>un boton</button>
-        </Item1>
-        <Item2 item={prod} />
-        <Item3 {...prod} />
-        <Item4 {...prod} destacado={false} />
-        <Item6 nombre="Remera" precio={5000} />
-        <Item5>
-          <h1>Este tiene children</h1>
-          <p>{prod.nombre}</p>
-          <p>{prod.precio}</p>
-        </Item5>
+        <Header />
+        <ItemListContainer
+          titulo={"Bienvenidos a la tienda de Embark"}
+          productos={arrayProductos}
+        />
+        <Footer />
       </div>
     </>
   );
